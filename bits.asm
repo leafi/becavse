f1651ac6e6681a7ea869c0887577b683bd97d131
leafi <leaf@leaf.sx>
2014-01-15 22:57:04 +0000

    strip out bits of bits.asm that referred to the old kernel

--- bits.asm
+++ bits.asm
@@ -21,13 +21,6 @@ global readthemsrthing
 global disable_local_apic
 global io_wait
 
-extern call_lua_int
-
-extern wait_for_out_data
-
-global a_irq4_handler
-extern irq4_handler
-
 simple:
   mov rax, 0x87654321
   ret
@@ -152,36 +145,6 @@ irq1_ps2_port1:
 	pop rax
 %endmacro
 
-%macro lirq 1
-	pushforccall
-	mov rdi, %1
-	call call_lua_int
-	popforccall
-	iretq
-%endmacro
-
-%macro dlirq 1
-	global irq%1
-	irq%1: lirq 0x20 + %1
-%endmacro
-
-dlirq 0
-dlirq 1
-dlirq 2
-dlirq 3
-dlirq 4
-dlirq 5
-dlirq 6
-dlirq 7
-dlirq 8
-dlirq 9
-dlirq 10
-dlirq 11
-dlirq 12
-dlirq 13
-dlirq 14
-dlirq 15
-
 irq1_ps2_port2:
 	push rax
 	mov al, [0xb8000]
@@ -197,28 +160,6 @@ irq1_ps2_port2:
 	; write master eoi
 	mov al, 0x20
 	out 0x20, al
-
-	pop rax
-	iretq
-
-a_irq4_handler:
-	push rax
-	mov al, [0xb8002]
-	inc al
-	mov [0xb8002], al
-
-	call irq4_handler
-
-	; eoi
-	mov al, 0x20
-	out 0x20, al
-
-	; shouldnt need this (slave eoi)
-	out 0xa0, al
-
-	; ??? shouldnt need this
-	mov rax, 0xfee000b0
-	mov [rax], rax
 
 	pop rax
 	iretq
